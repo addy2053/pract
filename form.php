afc9dcbb71348e87c695fcc256a9f1b39a9ac5c9
umar <addy2053@gmail.com>
2017-10-10 13:02:55 -0700

    INSERTION

--- form.php
+++ form.php
@@ -1,7 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="description" content="My name is umar" charset="UTF-8">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-theme.min.css">
+    <link rel="stylesheet" href="bootstrap.min.js">
+    <title>Title</title>
+</head>
+<body>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Umar
- * Date: 10/10/2017
- * Time: 9:49 AM
- */
+
+$con=mysqli_connect('localhost','root','','pract');
+if(!$con)
+{
+    die("Not Connect" .mysqli_connect_error());
+}
+
+//create table
+//$sql="CREATE TABLE tbl_std (
+//id INT NOT NULL PRIMARY KEY ,
+//name VARCHAR (50),
+//age INT
+//)";
+
+//drop table means delete table from databse
+//$sql="DROP TABLE persons";
+
+//if(mysqli_query($con,$sql))
+//{
+//    echo "table created";
+//}
+//else
+//{
+//    echo "table not created";
+//}
+?>
+
+
+<div class="container-fluid">
+<form action="process.php" method="post">
+
+
+    <label for="Id">Id :</label> <input type="text" name="id" size="10" maxlength="10">
+    <br>
+    <label for="Name">Name :</label> <input type="text" name="name" size="12" maxlength="20">
+    <br>
+    <label for="Age">Age :</label> <input type="text" name="age" size="12" maxlength="20">
+
+    <br>
+   <input type="submit" >
+
+
+
+</form>
+
+</div>
+</body>
+</html>
+
+
+
--- process.php
+++ process.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Umar
- * Date: 10/10/2017
- * Time: 9:47 AM
- */
+$con=mysqli_connect('localhost','root','','pract');
+if(!$con)
+{
+    die("Not Connect" .mysqli_connect_error());
+}
+if($_POST) {
+    $id=$_REQUEST['id'];
+    $name = $_REQUEST['name'];
+    $age = $_REQUEST['age'];
+
+$sql="INSERT INTO tbl_std
+VALUES('$id','$name','$age')";
+
+mysqli_query($con,$sql);
+}
+echo "Name:$name <br> Age: $age <br> id : $id";
+
+
+?>
