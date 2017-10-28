44ec9ec43a18e0ee4b4ecae35a2b27ef6ba6fa12
umar <addy2053@gmail.com>
2017-10-28 12:43:41 -0700

    before login

--- process.php
+++ process.php
@@ -4,22 +4,24 @@
 <html lang="en">
 <head>
     <meta name="description" content="My name is umar" charset="UTF-8">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.16/datatables.min.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.16/datatables.min.js"></script>
+    <script src="java.js"></script>
+
     <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap-theme.min.css">
     <link rel="stylesheet" href="bootstrap.min.js">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!--    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
 <!--    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">-->
 <!--    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">-->
 <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-          <script src="dataTables.bootstrap.js"></script>                                                                                                                                                                                                       <-->
-          <script src="jquery.dataTables.js"></script>                                                                                                                                                                                                       <-->
 
     <title>Title</title>
 </head>
 <body>
+
 <div class="container-fluid">
 <?php
 $con=mysqli_connect('localhost','root','','pract');
@@ -39,7 +41,7 @@ VALUES('$id','$name','$age')";
 }
 ?>
 
-    <table class="table table-bordered table-hover" >
+    <table class="table table-bordered table-hover" id="table">
 
     <thead>
         <tr><th >Id</th>
@@ -50,7 +52,7 @@ VALUES('$id','$name','$age')";
     <tbody>
     <?php
  function a(){echo "hey";}
-    $sql = "select *from tbl_std limit 0,2";
+    $sql = "select *from tbl_std";
 
     $result = mysqli_query($con, $sql);
     if (mysqli_num_rows($result) > 0) {
@@ -64,22 +66,6 @@ VALUES('$id','$name','$age')";
     }
 ?>
     </tbody>
-
-    <tfoot class="hide-if-no-paging">
-    <td colspan="3">
-
-<div class="col-lg-offset-5">
-<ul class="pagination">
-    <li class="" ><a href="">1</a></li>
-    <li ><a href="process.php">2</a></li>
-    <li><a href="">3</a></li>
-    <li><a href="">4</a></li>
-    <li><a href="">5</a></li>
-</ul>
-</div>
-    </td>
-
-    </tfoot>
 </table>
 
 </body>
